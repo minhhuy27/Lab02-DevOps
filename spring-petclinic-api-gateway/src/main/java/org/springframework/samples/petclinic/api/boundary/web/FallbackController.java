33776aa15181dca29ece058eaa8287ee47f7b0a6
minhhuy27 <dp1.2a4.minhhuy@gmail.com>
2025-12-22 23:15:45 +0700

    fix mapping

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/FallbackController.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/FallbackController.java
@@ -2,13 +2,14 @@ package org.springframework.samples.petclinic.api.boundary.web;
 
 import org.apache.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallbackController {
 
-    @PostMapping("/fallback")
+    @RequestMapping(path = "/fallback", method = {RequestMethod.GET, RequestMethod.POST})
     public ResponseEntity<String> fallback() {
         return ResponseEntity.status(HttpStatus.SC_SERVICE_UNAVAILABLE)
                 .body("Chat is currently unavailable. Please try again later.");
