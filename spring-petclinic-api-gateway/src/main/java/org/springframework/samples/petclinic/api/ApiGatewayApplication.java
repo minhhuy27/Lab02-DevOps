1b8eb5f69e36360e2c97f026f692fba73e23db13
minhhuy27 <dp1.2a4.minhhuy@gmail.com>
2025-12-22 15:36:50 +0700

    test

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
@@ -88,3 +88,4 @@ public class ApiGatewayApplication {
             .build());
     }
 }
+
--- spring-petclinic-discovery-server/src/main/java/org/springframework/samples/petclinic/discovery/DiscoveryServerApplication.java
+++ spring-petclinic-discovery-server/src/main/java/org/springframework/samples/petclinic/discovery/DiscoveryServerApplication.java
@@ -30,3 +30,4 @@ public class DiscoveryServerApplication {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 }
+
